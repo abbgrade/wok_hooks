241efee243f2e7b4e7df03c9d255c735aca4b9d5
abbgrade <steffen.kampmann@gmail.com>
2013-07-29 21:41:50 +0200

    add comments from email mailbox (imap)

--- __hooks__.py
+++ __hooks__.py
@@ -7,9 +7,10 @@ from hook_janitor import clean_temp_files
 from hook_distribute import distribute_output
 from hook_diaspora import add_diaspora_posts_to_microblog
 from hook_github import add_github_posts_to_microblog
+from hook_comments import add_mails_to_comments
 
 # The `hooks` dictionary that wok will import
 hooks = {
-    'site.start': [clean_temp_files, add_diaspora_posts_to_microblog, add_github_posts_to_microblog],
+    'site.start': [clean_temp_files, add_diaspora_posts_to_microblog, add_github_posts_to_microblog, add_mails_to_comments],
     'site.done': [distribute_output],
 }
---
+++ comments.py
@@ -0,0 +1,36 @@
+'''
+Created on 29.07.2013
+
+@author: steffen
+'''
+
+import logging
+import json
+
+class Post(object):
+
+    def __init__(self, slug, title, reference, time, author, author_email, content):
+        self.slug = slug
+        self.title = title
+        self.reference = reference
+        self.author = author
+        self.author_email = author_email
+        self.time = time
+        self.content = content
+
+    def save(self, content_dir):
+        with open('%s%s.md' % (content_dir, self.slug), 'w+') as fd:
+            def writeline(text):
+                try:
+                    fd.write(text.encode('utf8') + '\n')
+                except Exception as ex:
+                    logging.error(ex)
+            writeline('title: %s' % self.title)
+            writeline('slug: %s' % self.slug)
+            writeline('category: %s' % self.reference)
+            writeline('type: comment')
+            writeline('datetime: %s' % self.time.isoformat(' '))
+            writeline('author: %s' % self.author)
+            writeline('author_email: %s' % self.author_email)
+            writeline('---')
+            writeline(self.content)
